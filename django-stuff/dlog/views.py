c9dcea02212b9ba9e21ea837332d13703fa944c6
VilMe <vilma.medrano00@gmail.com>
2025-12-18 21:30:45 -0600

    add TODO task

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at my dog log index")
+    return HttpResponse("Hello, world. You're at my dog log index. WOOOOOOT! need to add mysql db next!!!!!")
