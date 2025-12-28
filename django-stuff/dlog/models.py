813b4a13d1fa8a7524d362e04b68fdb2d77a178b
VilMe <vilma.medrano00@gmail.com>
2025-12-28 17:28:44 -0600

    define models)

--- django-stuff/dlog/models.py
+++ django-stuff/dlog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# How many cups of kibble I fed goose
+class FoodAmount(models.Model):
+    feed_date = models.DateField()
+    cups_of_kibble = models.DecimalField(max_digits=4, decimal_places=1)
+
+# Notes and observations
+
+class Observation(models.Model):
+    weight = models.IntegerField()
+    note = models.TextField()
