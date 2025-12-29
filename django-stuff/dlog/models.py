f8897d7fd70e6b743d999e21ea0d7093d52029fc
VilMe <vilma.medrano00@gmail.com>
2025-12-29 10:07:30 -0600

    define string that returned by model for FoodAmount table

--- django-stuff/dlog/models.py
+++ django-stuff/dlog/models.py
@@ -5,6 +5,10 @@ class FoodAmount(models.Model):
     feed_date = models.DateField()
     cups_of_kibble = models.DecimalField(max_digits=4, decimal_places=1)
 
+
+    def __str__(self):
+        return self.feed_date, self.cups_of_kibble
+
 # Notes and observations
 
 class Observation(models.Model):
--- django-stuff/mysite/settings.py
+++ django-stuff/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dlog.apps.DlogConfig'
+    'dlog.apps.DlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
