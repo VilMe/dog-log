2e76d6f874ac8a1bedbf755ec5602c3d0c26d239
VilMe <vilma.medrano00@gmail.com>
2026-01-02 23:36:47 -0600

    start defining detail and results view

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at my dog log index. WOOOOOOT! need to add mysql db next!!!!!")
+
+
+def detail(request):
+    return HttpResponse("You're looking at dog log form")
+
+
+def results(request):
+    return HttpResponse("You're looking at dog log results table ")
+
