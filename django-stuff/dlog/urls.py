7d3e5b14f384b2fc5f96acad635b89200e86327f
VilMe <vilma.medrano00@gmail.com>
2026-01-02 23:42:05 -0600

    wire views to urls

--- django-stuff/dlog/urls.py
+++ django-stuff/dlog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns: list = [
+    # ex: /dlog/
     path("", views.index, name="index"), 
+    path("form/", views.detail, name="detail"),
+    path("results/", views.results, name="results"),
 ]
