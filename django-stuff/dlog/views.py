cfa244bae643ba95562747ac36f26f1ca741308e
VilMe <vilma.medrano00@gmail.com>
2026-01-04 12:46:25 -0600

    update gitignore and views

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at my dog log index. WOOOOOOT! need to add mysql db next!!!!!")
+    return HttpResponse("Hello, world. You're at my dog log index. WOOOOOOT!")
 
 
 def detail(request):
