34067af5cf4a264447ce69b922f599c1e936d54c
VilMe <vilma.medrano00@gmail.com>
2026-01-04 18:11:32 -0600

    update view to use dlog/index.html template

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import FoodAmount
 
 def index(request):
     latest_food_amounts_list = FoodAmount.objects.order_by("-feed_date")[:5]
-    output = ", ".join([f.cups_of_kibble for f in latest_food_amounts_list])
-    return HttpResponse(output)
+    template = loader.get_template("dlog/index.html")
+    context = {"latest_food_amounts_list": latest_food_amounts_list}
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request):
