1d6b31cd5bf05155635a5aa65dedd256793b3f0e
VilMe <vilma.medrano00@gmail.com>
2026-01-08 20:02:35 -0600

    add namespace to urls configuration in mysite

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from .models import FoodAmount
 
 def index(request):
-    latest_food_amounts_list = FoodAmount.objects.order_by("feed_date")[:5]
+    latest_food_amounts_list = FoodAmount.objects.order_by("-feed_date")[:5]
     template = loader.get_template("dlog/index.html")
     context = {"latest_food_amounts_list": latest_food_amounts_list}
     return HttpResponse(template.render(context, request))
