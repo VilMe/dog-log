5bfca8b1e3509255ed32bae8dad32513619547a9
VilMe <vilma.medrano00@gmail.com>
2026-01-08 21:31:26 -0600

    define template for form to record Goose information

--- django-stuff/dlog/urls.py
+++ django-stuff/dlog/urls.py
@@ -6,6 +6,7 @@ app_name = "dlog"
 urlpatterns: list = [
     # ex: /dlog/
     path("", views.index, name="index"), 
+    # ex: /dlog/form
     path("form/", views.form, name="form"),
     path("results/", views.results, name="results"),
 ]
