95f63ad8f5dad2e55de08e507bba9b580e257e15
VilMe <vilma.medrano00@gmail.com>
2026-01-17 19:57:04 -0600

    update html of form to resolve csrf issue, csrf issue not resolved

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -18,8 +18,8 @@ def form(request):
 
 def record(request):
     if request.method == "POST":
-        feed_date = request.POST.get('feed_date')
-        cups_of_kibble = request.POST.get('cups_of_kibble')
+        feed_date = request.POST('feed_date')
+        cups_of_kibble = request.POST('cups_of_kibble')
         feed_info = FoodAmount(feed_date=feed_date, cups_of_kibble=cups_of_kibble)
         feed_info.save
     return render(request, "You're looking at dog log record function")
