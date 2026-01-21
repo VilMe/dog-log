7c5bde67c80ce5adda743ea0d18231b208176015
VilMe <vilma.medrano00@gmail.com>
2026-01-20 22:02:04 -0600

    update form view to render request, now cookie is being created and passed back but data is not being submitted to database

--- django-stuff/dlog/views.py
+++ django-stuff/dlog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import FoodAmount,Observation
 
@@ -12,14 +13,14 @@ def index(request):
 
 
 def form(request):
-    template = loader.get_template("dlog/form.html")
+    # template = loader.get_template("dlog/form.html")
     return render(request, "dlog/form.html")
 
 
 def record(request):
-    feed_date = request.POST('feed_date')
-    cups_of_kibble = request.POST('cups_of_kibble')
+    feed_date = request.POST.get('feed_date')
+    cups_of_kibble = request.POST.get('cups_of_kibble')
     feed_info = FoodAmount(feed_date=feed_date, cups_of_kibble=cups_of_kibble)
     feed_info.save
-    return HttpResponseRedirect("You have submitted data, woohoo!!")
+    return HttpResponseRedirect("/dlog/")
 
